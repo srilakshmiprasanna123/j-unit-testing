fb7c9ba799371796396c01fe390dd3de46cfd8fd
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-04-03 17:09:39 +0530

    added password of all cases

--- src/main/java/UserRegistrationMain.java
+++ src/main/java/UserRegistrationMain.java
@@ -40,4 +40,46 @@ public class UserRegistrationMain {
         matcher = pattern.matcher(PhoneNo);
         return matcher.matches();
     }
+    /* method loginValidation to validate password format 1
+   @param Password return boolean value
+ */
+    public boolean loginValidation(String Password) {
+        String regexPassword = "[A-Za-z0-9]{8,}";
+        pattern = Pattern.compile(regexPassword);
+        matcher = pattern.matcher(Password);
+        return matcher.matches();
+    }
+
+    /* method loginValidation to validate password format 2
+      Password should have at least 1 or more digits
+      @param Password return boolean value
+    */
+    public boolean loginValidation2(String Password) {
+        String regexPassword = "[A-Za-z0-9]{8,}";
+        pattern = Pattern.compile(regexPassword);
+        matcher = pattern.matcher(Password);
+        return matcher.matches();
+    }
+
+    /* method loginValidation to validate password format 3
+       Password should have at least 1 or more digits
+       @param Password return boolean value
+     */
+    public boolean loginRule3Validation(String name) {
+        String regex = "(?=.*[A-Z]){1}(?=.*[0-9])(?=.*[a-z]).{8,}$";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(name);
+        return matcher.matches();
+    }
+
+    /* method loginValidation to validate password format 4
+       Password should have 1 special character
+       @param Password return boolean value
+    */
+    public boolean loginRule4Validation(String Password) {
+        String regexPassword4 = "^(?=.*[A-Z]){1}(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%!]).{8,}$";
+        pattern = Pattern.compile(regexPassword4);
+        matcher = pattern.matcher(Password);
+        return matcher.matches();
+    }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -33,4 +33,43 @@ public class UserRegistrationTest {
         boolean result = userRegistration.numberValidation("91 08450911877");
         Assert.assertFalse(result);
     }
+    @Test
+    public void giveLoginValue_WhenProper_ShouldReturnTrue(){
+        boolean result = userRegistration.loginValidation("Asbmsopnm");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenLoginValues_WhenNotProper_ShouldReturnFalse() {
+        boolean result = userRegistration.loginValidation("Asjspnm");
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void givenRule2Value_WhenProper_ShouldReturnTrue(){
+        boolean result = userRegistration.loginValidation2("Logaqaaopnm");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenRule2Values_WhenNotProper_ShouldReturnFalse(){
+        boolean result = userRegistration.loginValidation2("addqeghopnm");
+    }
+    @Test
+    public void givenRule3Value_WhenProper_ShouldReturnTrue(){
+        boolean result = userRegistration.loginRule3Validation("Laoqo1rpnm");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenRule3Values_WhenNotProper_ShouldReturnFalse(){
+        boolean result = userRegistration.loginRule3Validation("aqLawhrpnm");
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void givenRule4Values_WhenProper_ShouldReturnTrue(){
+        boolean result = userRegistration.loginRule4Validation("adqe1aaL@fp");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenRule4Values_WhenNotProper_ShouldReturnFalse(){
+        boolean result = userRegistration.loginRule4Validation("awwfleropnm");
+        Assert.assertFalse(result);
+    }
 }
